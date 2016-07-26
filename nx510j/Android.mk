1389c92eb9a69ccaec4b68171f2a8e4e7011ac10
dianlujitao <dianlujitao@gmail.com>
2016-07-26 22:37:24 +0800

    nx510j: Add radio filesmap

--- nx510j/Android.mk
+++ nx510j/Android.mk
@@ -350,9 +350,12 @@ $(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib/egl && pushd $(PRODUCT_OUT)/sy
 $(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib64/egl && pushd $(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 
 ifeq ($(LOCAL_PATH)/radio, $(wildcard $(LOCAL_PATH)/radio))
-RADIO_FILES := $(shell cd $(LOCAL_PATH)/radio ; ls)
-$(foreach f, $(RADIO_FILES), \
+
+RADIO_FILES := $(wildcard $(LOCAL_PATH)/radio/*)
+$(foreach f, $(notdir $(RADIO_FILES)), \
     $(call add-radio-file,radio/$(f)))
+$(call add-radio-file,../../../device/zte/nx510j/radio/filesmap)
+
 endif
 
 endif
